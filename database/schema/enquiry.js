0a5b2e7ac3b0b43a0d2c1642ef652216b888682c
Shivam <coolsan929@gmail.com>
2025-01-26 23:06:47 +0530

    commit

--- database/schema/enquiry.js
+++ database/schema/enquiry.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 const { buffer } = require("stream/consumers");
 
 const W_Mark_Enquiry_Schema = new mongoose.Schema({
+
   loanAmount: {
     type: String,
   },
+
   phoneNo: {
     type: String,
   },
+
   city: {
     type: String,
   },
+
   status: {
     type: String,
   },
@@ -18,10 +22,12 @@ const W_Mark_Enquiry_Schema = new mongoose.Schema({
   dateOfFormSubmission: {
     type: String,
   },
+
 });
 
 const NewBankOffers = new mongoose.model(
   "W_Mark_Enquiry",
   W_Mark_Enquiry_Schema
 );
+
 module.exports = NewBankOffers;
